affa50069fa790d58e7ad127db1b85d2e0dedf37
Vaneshik <maksimchusovlyanov@yandex.ru>
2024-06-03 17:19:51 +0300

    meow1

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -4,11 +4,11 @@ import java.net.InetSocketAddress;
 
 public class Main {
     public static void main(String[] args) {
-//        Server server = new Server(new InetSocketAddress(12727));
-//        server.run(args);
+        Server server = new Server(new InetSocketAddress(12727));
+        server.run(args);
 
-        Server server = new Server(new InetSocketAddress("localhost", 8000));
-        server.run(new String[]{"data.xml"});
+//        Server server = new Server(new InetSocketAddress("localhost", 8000));
+//        server.run(new String[]{"data.xml"});
 //        Строчки для теста на локалхосте. Для гелиуса достаточно указать только порт
     }
 }
